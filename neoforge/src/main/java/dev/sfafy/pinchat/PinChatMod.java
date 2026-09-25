6bf89b45a05b63fad4c1b58833f3e6ec75e4de46
ivanmikhaylov1 <ivanmikhaylov123@gmail.com>
2026-09-25 17:49:32 +0300

    fix: register NeoForge client handlers only on client

--- neoforge/src/main/java/dev/sfafy/pinchat/PinChatMod.java
+++ neoforge/src/main/java/dev/sfafy/pinchat/PinChatMod.java
@@ -3,6 +3,8 @@ package dev.sfafy.pinchat;
 import dev.sfafy.pinchat.keybindings.PinChatKeyBindings;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +15,11 @@ public class PinChatMod {
   public static final String MOD_ID = "pinchat";
 
   public PinChatMod(IEventBus modBus) {
-    modBus.addListener(ClientSetup::onClientSetup);
-    modBus.addListener(PinChatKeyBindings::registerKeyMappings);
-    modBus.addListener(PinnedHudRenderer::registerOverlays);
-    NeoForge.EVENT_BUS.addListener(NeoClientEvents::onKeyInput);
+    if (FMLEnvironment.getDist() == Dist.CLIENT) {
+      modBus.addListener(ClientSetup::onClientSetup);
+      modBus.addListener(PinChatKeyBindings::registerKeyMappings);
+      modBus.addListener(PinnedHudRenderer::registerOverlays);
+      NeoForge.EVENT_BUS.addListener(NeoClientEvents::onKeyInput);
+    }
   }
 }
